73632d83918df7446cae4c6e8d6e1c3d4015ec2a
Simon Kornblith <simon@simonster.com>
2010-10-15 06:41:28 +0000

    move OpenOffice.org preferences to Zotero pane

---
+++ chrome/common.js
@@ -0,0 +1,167 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+	
+	Copyright (c) 2009  Zotero
+	                    Center for History and New Media
+						George Mason University, Fairfax, Virginia, USA
+						http://zotero.org
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
+	
+var appInfo = Components.classes["@mozilla.org/xre/app-info;1"].
+						 getService(Components.interfaces.nsIXULAppInfo);
+if(appInfo.platformVersion[0] == 2) {
+	Components.utils.import("resource://gre/modules/AddonManager.jsm");
+} else {
+	var AddonManager = false;
+}
+
+var ZoteroPluginInstaller = function(addon, failSilently, force) {
+	this._addon = addon;
+	this._failSilently = failSilently;
+	this._force = force;
+	
+	this.prefService = Components.classes["@mozilla.org/preferences-service;1"].
+			getService(Components.interfaces.nsIPrefBranch);
+	
+	var me = this;
+	var extensionIDs = [this._addon.EXTENSION_ID].concat([req.id for each(req in this._addon.REQUIRED_ADDONS)]);
+	if(AddonManager) {
+		AddonManager.getAddonsByIDs(extensionIDs, function(addons) {
+			me._addons = addons;
+			me._addonInfoAvailable();
+		});
+	} else {
+		var extMan = Components.classes['@mozilla.org/extensions/manager;1'].
+								getService(Components.interfaces.nsIExtensionManager)
+		this._addons = [extMan.getItemForID(id) for each(id in extensionIDs)];
+		this._addonInfoAvailable();
+	}
+}
+
+ZoteroPluginInstaller.prototype = {
+	"_errorDisplayed":false,
+	
+	"_addonInfoAvailable":function() {
+		this._checkVersions();
+		
+		this._version = this._addons[0].version;
+		try {
+			this._addon.verifyNotCorrupt(this);
+		} catch(e) {
+			return;
+		}
+		
+		if(this._force || (this.prefService.getCharPref(this._addon.EXTENSION_PREF_BRANCH+".version") != this._version && document.getElementById("appcontent"))) {
+			var me = this;
+			this._progressWindow = window.openDialog("chrome://"+this._addon.EXTENSION_DIR+"/content/progress.xul", "",
+							"chrome,resizable=no,close=no,centerscreen");
+			this._progressWindow.addEventListener("load", function() { me._firstRunListener() }, false);
+		}
+	},
+	
+	"isInstalled":function() {
+		return this.prefService.getCharPref(this._addon.EXTENSION_PREF_BRANCH+".version") == this._version;
+	},
+	
+	"getAddonPath":function(addonID) {
+		if(AddonManager) {
+			for each(var addon in this._addons) {
+				if(addon.id == addonID) {
+					return this._addons[0].getResourceURI().
+						QueryInterface(Components.interfaces.nsIFileURL).file;
+				}
+			}
+		} else {
+			return Components.classes["@mozilla.org/extensions/manager;1"].
+				getService(Components.interfaces.nsIExtensionManager).
+				getInstallLocation(addonID).
+				getItemLocation(addonID);
+		}
+	},
+	
+	"setProgressWindowLabel":function(value) {
+		this._progressWindowLabel.value = value;
+	},
+	
+	"closeProgressWindow":function(value) {
+		if(this._progressWindow) this._progressWindow.close();
+	},
+	
+	"success":function() {
+		this.prefService.setCharPref(this._addon.EXTENSION_PREF_BRANCH+".version", this._version);
+		this.closeProgressWindow();
+		if(this._force) {
+			Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+				.getService(Components.interfaces.nsIPromptService)
+				.alert(window, this._addon.EXTENSION_STRING,
+				'Installation was successful.');
+		}
+	},
+	
+	"error":function(error) {
+		this.closeProgressWindow();
+		if(this._failSilently) return;
+		if(this._errorDisplayed) return;
+		Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+			.getService(Components.interfaces.nsIPromptService)
+			.alert(null, this._addon.EXTENSION_STRING,
+			'Installation could not be completed because an error occurred. Please ensure that '+this._addon.APP+' is closed, and then restart Firefox.');
+	},
+	
+	"_firstRunListener":function() {
+		this._progressWindowLabel = this._progressWindow.document.getElementById("progress-label");
+		this._progressWindowLabel.value = "Installing "+this._addon.EXTENSION_STRING+"...";
+		var me = this;
+		window.setTimeout(function() {
+			me._progressWindow.focus();
+			window.setTimeout(function() {
+				me._progressWindow.focus();
+				Zotero.debug("about to install");
+				Zotero.debug(me.prefService.toSource());
+				try {
+					me._addon.install(me);
+				} catch(e) {
+					me.error();
+					throw e;
+				}
+			}, 500);
+		}, 100);
+	},
+	
+	"_checkVersions":function() {
+		for(var i=0; i<this._addon.REQUIRED_ADDONS.length; i++) {
+			var checkAddon = this._addon.REQUIRED_ADDONS[i];
+			
+			// check Zotero version
+			try {
+				var comp = Components.classes["@mozilla.org/xpcom/version-comparator;1"]
+					.getService(Components.interfaces.nsIVersionComparator)
+					.compare(this._addons[i+1].version, checkAddon.minVersion);
+			} catch(e) {
+				var comp = -1;
+			}
+			
+			if(comp < 0) {
+				var err = 'This version of '+this._addon.EXTENSION_STRING+' requires '+checkAddon.name+' '+checkAddon.minVersion+
+					' or later to run. Please download the latest version of '+checkAddon.name+' from '+checkAddon.url+'.';
+				this.error(err);
+				if(!this._failSilently) throw err;
+			}
+		}
+	}
+}
---
+++ chrome/openOffice.js
@@ -0,0 +1,300 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+	
+	Copyright (c) 2009  Zotero
+	                    Center for History and New Media
+						George Mason University, Fairfax, Virginia, USA
+						http://zotero.org
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
+
+var ZoteroOpenOfficeIntegration = new function() {
+	const URE_PREF = "extensions.zoteroOpenOfficeIntegration.urePath";
+	const SOFFICE_PREF = "extensions.zoteroOpenOfficeIntegration.sofficePath";
+	
+	this.EXTENSION_STRING = "Zotero OpenOffice Integration";
+	this.EXTENSION_ID = "zoteroOpenOfficeIntegration@zotero.org";
+	this.EXTENSION_PREF_BRANCH = "extensions.zoteroOpenOfficeIntegration";
+	this.EXTENSION_DIR = "zotero-openoffice-integration";
+	this.APP = 'OpenOffice.org';
+	
+	this.REQUIRED_ADDONS = [{
+		name: "Zotero",
+		url: "zotero.org",
+		id: "zotero@chnm.gmu.edu",
+		minVersion: "2.1a1.SVN"
+	}];
+	
+	var zoteroPluginInstaller, pathToAddon, installing;
+	
+	this.verifyNotCorrupt = function() {}
+	
+	this.install = function(zpi) {
+		if(installing) throw this.EXTENSION_STRING+" installation already in progress; aborting";
+		var installing = true;
+		
+		zoteroPluginInstaller = zpi;
+		
+		if(zoteroPluginInstaller.prefService.getCharPref(SOFFICE_PREF) == "" ||
+		   zoteroPluginInstaller.prefService.getCharPref(URE_PREF) == "") {
+			zoteroPluginInstaller.setProgressWindowLabel("Detecting OpenOffice.org Paths...");
+			detectPaths();
+		}
+		
+		pathToAddon = zoteroPluginInstaller.getAddonPath(this.EXTENSION_ID);
+		
+		installComponents(function(success) {
+			if(!success) {
+				error();
+				installing = false;
+				throw "An error occurred running unopkg";
+			}
+			zoteroPluginInstaller.closeProgressWindow();
+			testInstall();
+			installing = false;
+			zoteroPluginInstaller.success();
+		});
+	}
+	
+	this.selectSoffice = function selectSoffice(parentDirectory) {
+		var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(Components.interfaces.nsIFilePicker);
+		fp.init(window, "Select the directory containing the soffice executable", Components.interfaces.nsIFilePicker.modeGetFolder);
+		if(Zotero.isWin) {
+			fp.appendFilter("Executable File", "*.exe");
+		} else {
+			fp.appendFilter("Executable File", "*");
+		}
+		if(parentDirectory) fp.displayDirectory = parentDirectory;
+		if(fp.show() != Components.interfaces.nsIFilePicker.returnOK) throw "User cancelled Zotero OpenOffice Integration install";
+		var ioService = Components.classes["@mozilla.org/network/io-service;1"].
+			getService(Components.interfaces.nsIIOService);
+		zoteroPluginInstaller.prefService.setCharPref(SOFFICE_PREF, ioService.newFileURI(fp.file).spec);
+	}
+	
+	this.selectURE = function selectURE(parentDirectory) {
+		var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(Components.interfaces.nsIFilePicker);
+		fp.init(window, "Select the directory containing the URE JAR files", Components.interfaces.nsIFilePicker.modeGetFolder);
+		fp.appendFilter("JAR File", "*.jar");
+		if(parentDirectory) fp.displayDirectory = parentDirectory;
+		if(fp.show() != Components.interfaces.nsIFilePicker.returnOK) throw "User cancelled Zotero OpenOffice Integration install";
+		var ioService = Components.classes["@mozilla.org/network/io-service;1"].
+			getService(Components.interfaces.nsIIOService);
+		zoteroPluginInstaller.prefService.setCharPref(URE_PREF, ioService.newFileURI(fp.file).spec);
+	}
+	
+	this.detectPaths = function detectPaths() {
+		try {
+			var OPENOFFICE_LOCATIONS = {
+				Mac:[
+					"/Applications/OpenOffice.org.app",
+					"/Applications/NeoOffice.app",
+					"/Applications/OpenOffice.org 2.4.app"
+				],
+				Win:[
+					"C:\\Program Files\\OpenOffice.org 3",
+					"C:\\Program Files (x86)\\OpenOffice.org 3",
+					"C:\\Program Files\\OpenOffice.org 2.4",
+					"C:\\Program Files (x86)\\OpenOffice.org 2.4",
+					"C:\\Program Files\\OpenOffice.org 2",
+					"C:\\Program Files (x86)\\OpenOffice.org 2"
+				],
+				Other:[
+					"/usr/local/opt/openoffice.org3.1",
+					"/opt/openoffice.org3.1",
+					"/opt/openoffice.org3",
+					"/usr/lib64/ooo3",
+					"/usr/lib/ooo3",
+					"/usr/lib64/openoffice.org3",
+					"/usr/lib/openoffice.org3",
+					"/usr/local/opt/openoffice.org3",
+					"/opt/openoffice.org3",
+					"/usr/lib/openoffice",
+					"/usr/local/opt/openoffice.org2",
+					"/opt/openoffice.org2",
+					"/usr/local/opt/openoffice.org2.4",
+					"/opt/openoffice.org2.4"
+				]
+			};
+			
+			var SOFFICE_LOCATIONS = {
+				Mac:[
+					"Contents/MacOS/soffice"
+				],
+				Win:[
+					"program\\soffice.exe"
+				],
+				Other:[
+					"program/soffice"
+				]
+			};
+			
+			var URE_LOCATIONS = {
+				Mac:[
+					"Contents/basis-link/ure-link/share/java",
+					"Contents/MacOS/classes"
+				],
+				Win:[
+					"URE\\java",
+					"program\\classes"
+				],
+				Other:[
+					"basis-link/ure-link/share/java",
+					"program/classes"
+				]
+			};
+			
+			if(Zotero.isMac) {
+				var platform = "Mac";
+			} else if(Zotero.isWin) {
+				var platform = "Win";
+			} else {
+				var platform = "Other";
+			}
+			
+			// look in obvious places for OpenOffice application
+			var appLocations = OPENOFFICE_LOCATIONS[platform];
+			var bestFile = null;
+			var i = 0;
+			do {		
+				var file = Components.classes["@mozilla.org/file/local;1"].
+					createInstance(Components.interfaces.nsILocalFile);
+				file.followLinks = true;
+				file.initWithPath(appLocations.shift());
+				if(file.exists() && (!bestFile || !bestFile.equals(file))) {
+					i++;
+					if(!bestFile) bestFile = file;
+				}
+			} while(appLocations.length);
+				
+			// if we have 0 or >1 found OpenOffice installations, ask user to pick
+			if(i != 1) {
+				var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(Components.interfaces.nsIFilePicker);
+				
+				if(Zotero.isMac) {
+					fp.init(window, "Select the OpenOffice or NeoOffice application", Components.interfaces.nsIFilePicker.modeOpen);
+					fp.appendFilter("Mac OS X Application Bundle", "*.app");
+				} else {
+					fp.init(window, "Select the OpenOffice installation directory", Components.interfaces.nsIFilePicker.modeGetFolder);
+				}
+				
+				if(bestFile) fp.displayDirectory = bestFile.parent;
+				if(fp.show() != Components.interfaces.nsIFilePicker.returnOK) throw "User cancelled Zotero OpenOffice Integration install";
+				bestFile = fp.file;
+			}
+			
+			// look for soffice executable and URE libs
+			var sofficePath = null;
+			var urePath = null;
+			for each(var relPath in SOFFICE_LOCATIONS[platform]) {
+				var testPath = bestFile.clone().QueryInterface(Components.interfaces.nsILocalFile);
+				testPath.appendRelativePath(relPath);
+					Zotero.debug(testPath.path);
+				if(testPath.exists()) {
+					sofficePath = testPath;
+					break;
+				}
+			}
+			for each(var relPath in URE_LOCATIONS[platform]) {
+				var testPath = bestFile.clone().QueryInterface(Components.interfaces.nsILocalFile);
+				testPath.appendRelativePath(relPath);
+				if(testPath.exists()) {
+					urePath = testPath;
+					break;
+				}
+			}
+			
+			// if executable or URE location are still missing, ask the user to locate them
+			if(!sofficePath || !urePath) {
+				var parentDirectory = bestFile.clone();
+				if(!parentDirectory.isDirectory()) parentDirectory = parentDirectory.parent;
+			}
+			
+			var prefService = Components.classes["@mozilla.org/preferences-service;1"].
+				getService(Components.interfaces.nsIPrefBranch);
+			var ioService = Components.classes["@mozilla.org/network/io-service;1"].
+				getService(Components.interfaces.nsIIOService);
+			if(!sofficePath) {
+				sofficePath = selectSoffice(parentDirectory);
+			} else {
+				prefService.setCharPref(SOFFICE_PREF, ioService.newFileURI(sofficePath.parent).spec);
+			}
+			
+			if(!urePath) {
+				urePath = selectURE(parentDirectory);
+			} else {
+				prefService.setCharPref(URE_PREF, ioService.newFileURI(urePath).spec);
+			}
+		} catch(e) {
+			installing = false;
+			if(e != "User cancelled Zotero OpenOffice Integration install") {
+				error();
+			}
+			throw e;
+		}
+	}
+	
+	function installComponents(callback) {
+		var ioService = Components.classes["@mozilla.org/network/io-service;1"].
+			getService(Components.interfaces.nsIIOService);
+		var executableDir = ioService.getProtocolHandler("file").
+			QueryInterface(Components.interfaces.nsIFileProtocolHandler).
+			getFileFromURLSpec(zoteroPluginInstaller.prefService.getCharPref(SOFFICE_PREF));
+		
+		// now install the oxt using unopkg
+		var oxt = pathToAddon.clone();
+		oxt.append("install");
+		oxt.append("Zotero_OpenOffice_Integration.oxt");
+		
+		if(Zotero.isWin) {
+			executableDir.append("unopkg.exe");
+		} else {
+			executableDir.append("unopkg");
+		}
+		
+		var proc = Components.classes["@mozilla.org/process/util;1"].
+				createInstance(Components.interfaces.nsIProcess);
+		proc.init(executableDir);
+		
+		zoteroPluginInstaller.setProgressWindowLabel("Removing Old Zotero OpenOffice.org Extension...");
+		
+		proc.runAsync(["remove", "org.Zotero.integration.openoffice"], 2, {"observe":function() {
+			zoteroPluginInstaller.setProgressWindowLabel("Adding Zotero OpenOffice.org Extension...");
+			proc.runAsync(["add", oxt.path], 2, {"observe":function(process, topic) {
+				callback(topic == "process-finished" && !process.exitValue);
+			}});
+		}});
+	}
+	
+	function testInstall() {
+		// test install
+		try {
+			java.lang;
+		} catch(e) {
+			var err = 'Zotero OpenOffice Integration was successfully installed, but it will not operate properly because Java is not installed or is not operational. You can test Firefox\'s Java support by going to www.javatester.org.';
+			if(window.navigator.platform.substr(0, 5) == "Linux") {
+				err += "\n\nPlease ensure that an up-to-date version of the Sun Java Plug-in (e.g., sun-java6-plugin) is installed and try again.";
+			} else if(window.navigator.platform.substr(0, 3) == "Win") {
+				err += "\n\nIf you are running Firefox on a 64-bit version of Windows, try disabling the \"next-generation Java plug-in\" in the Java control panel. In Windows Vista, open the control panel, switch to Classic View, and open \"View 32-bit Control Panel Items.\" In Windows 7, select view by \"Small icons.\" The setting is located in the \"Advanced\" tab, under \"Java plug-in\".";
+			}
+			
+			error(err);
+			
+			installing = false;
+			throw e;
+		}
+	}
+}
