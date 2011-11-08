4ee917154fbcad0a331c151c5919def1b0d51d26
Simon Kornblith <simon@simonster.com>
2011-11-08 01:51:14 -0500

    Basic README

---
+++ README.mk
@@ -0,0 +1,12 @@
+Zotero LibreOffice Integration
+==============================
+
+Zotero LibreOffice Integration comprises extensions for LibreOffice/OpenOffice.org/NeoOffice extension and Firefox that allow the two to communicate. 
+
+Build Requirements
+--------
+
+*  LibreOffice 3 and SDK from [libreoffice.org](http://www.libreoffice.org)
+   (other versions may work as well)
+*  Eclipse
+*  [OpenOffice.org Eclipse Integration](http://cedric.bosdonnat.free.fr/wordpress/?page_id=11&lp_lang_view=en)
