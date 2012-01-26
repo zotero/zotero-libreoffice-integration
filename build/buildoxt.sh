7f6b67b0f8c36f6095ab54b266f0a8038fa60522
Simon Kornblith <simon@simonster.com>
2012-01-25 23:07:43 -0500

    Fix types.rdb building in buildoxt.sh

--- build/buildoxt.sh
+++ build/buildoxt.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Compile definitions
-#cd idl
-#/opt/libreoffice/basis3.3/sdk/bin/idlc -O../build/urd/org/zotero/integration/ooo -I/opt/libreoffice/basis3.3/sdk/idl org/zotero/integration/ooo/*idl
-#cd ..
-#/opt/libreoffice/ure/bin/regmerge build/urd/org/zotero/integration/ooo/*urd /UCR types.rdb
-#cd build
-#/opt/libreoffice/basis3.3/sdk/bin/javamaker -BUCR ../types.rdb /opt/libreoffice/ure/share/misc/types.rdb
-#cd ..
+LO_DIR=/opt/libreoffice3.4
+BASIS_DIR="$LO_DIR"/basis3.4
+
+cd idl
+"$BASIS_DIR"/sdk/bin/idlc -O../build/urd/org/zotero/integration/ooo -I"$BASIS_DIR"/sdk/idl org/zotero/integration/ooo/*idl
+cd ..
+"$LO_DIR"/ure/bin/regmerge types.rdb build/urd/org/zotero/integration/ooo/*urd /UCR 
+cd build
+"$BASIS_DIR"/sdk/bin/javamaker -BUCR ../types.rdb "$LO_DIR"/ure/share/misc/types.rdb
+cd ..
 
 # Copy external jars
 cp -r lib/*jar oxt/external_jars
