51b80a8a36c8b2c72ee109c10efe5110464e4354
Simon Kornblith <simon@simonster.com>
2012-05-31 20:33:02 -0400

    Add missing file from 44660cb846ca70bf4824bbd15638dc51652cbe81

---
+++ build/source/org/zotero/integration/ooo/comp/MarkManager.java
@@ -0,0 +1,56 @@
+package org.zotero.integration.ooo.comp;
+
+import java.util.Hashtable;
+
+import com.sun.star.container.XNamed;
+import com.sun.star.uno.UnoRuntime;
+
+public class MarkManager {
+	private Hashtable<String, ReferenceMark> mMarks;
+	private Document mDoc;
+	
+	MarkManager(Document aDoc) {
+		mDoc = aDoc;
+		mMarks = new Hashtable<String, ReferenceMark>();
+	}
+	
+	void renameMark(String aOldName, String aNewName) {
+		ReferenceMark oldMark = mMarks.get(aOldName);
+		mMarks.remove(aOldName);
+		mMarks.put(aNewName, oldMark);
+	}
+	
+    ReferenceMark getMark(Object aMark, String aFieldType) {
+    	if(aMark == null) {
+    		return null;
+    	}
+    	
+    	XNamed named = (XNamed) UnoRuntime.queryInterface(XNamed.class, aMark);
+		String name = named.getName();
+		
+		// Return hashed mark if it exists
+		ReferenceMark retMark = mMarks.get(name);
+		if(retMark != null) {
+			return retMark;
+		}
+		
+		// Create new mark
+		for(String prefix : Document.PREFIXES) {
+			if(name.contains(prefix)) {
+				try {
+					if(aFieldType.equals("ReferenceMark")) {
+						retMark = new ReferenceMark(mDoc, named, name);
+					} else if(aFieldType.equals("Bookmark")) {
+						retMark = new Bookmark(mDoc, named, name);
+					} else {
+						return null;
+					}
+					mMarks.put(name, retMark);
+				} catch (IllegalArgumentException e) {}
+				return retMark;
+			}
+		}
+		
+		return null;
+    }
+}
