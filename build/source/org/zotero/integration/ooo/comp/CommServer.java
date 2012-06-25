5df84852aa062e8d6824e48c29e7553eac83fc97
Simon Kornblith <simon@simonster.com>
2012-06-25 17:58:27 -0400

    Don't hang after showing incompatible version error message

--- build/source/org/zotero/integration/ooo/comp/CommServer.java
+++ build/source/org/zotero/integration/ooo/comp/CommServer.java
@@ -12,71 +12,71 @@ public class CommServer implements Runnable {
 	 * Called when new data is received to handle interfacing with Java methods
 	 */
 	public void run() {
-		try {
-			Socket socket;
-			CommFrame deferredFrame = null;
-			
-			while(true) {
-				DataOutputStream outputStream;
-				try {
-					// Open socket and output stream
-					socket = new Socket("127.0.0.1", 23116);
-					socket.setTcpNoDelay(true);
-					outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream(), 16384));
-					
-					// Start another thread to read from the socket
-					new Thread(new CommReader(socket)).start();
-				} catch (ConnectException e) {
-					try {
-						socket = new Socket("127.0.0.1", 19876);
-						socket.close();
-						Comm.showError(Comm.OLD_VERSION_STRING, null);
-					} catch(Exception e1) {
-						Comm.showError(Comm.COMMUNICATION_ERROR_STRING, null);
-					}
-					return;
-				} catch (Exception e) {
-					Comm.showError(Document.getErrorString(e), e);
-					return;
-				}
+		Socket socket;
+		CommFrame deferredFrame = null;
+		
+		while(true) {
+			DataOutputStream outputStream;
+			try {
+				// Open socket and output stream
+				socket = new Socket("127.0.0.1", 23116);
+				socket.setTcpNoDelay(true);
+				outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream(), 16384));
 				
+				// Start another thread to read from the socket
+				new Thread(new CommReader(socket)).start();
+			} catch (ConnectException e) {
 				try {
-					// Start write loop
-					while(true) {
-						CommFrame frame;
-						if(deferredFrame == null) {
-							try {
-								frame = Comm.writeQueue.take();
-							} catch (InterruptedException e) {
-								continue;
-							}
-						} else {
-							frame = deferredFrame;
-							deferredFrame = null;
-						}
-						
-						byte[] payload = frame.getBytes();
-						//ZoteroOpenOfficeIntegrationImpl.debugPrint("Writing "+new String(payload));
-						
-						try {
-							outputStream.writeInt(frame.getTransactionID());
-							outputStream.writeInt(payload.length);
-							outputStream.write(payload);
-							outputStream.flush();
-						} catch(SocketException e) {
-							deferredFrame = frame;
-							break;
-						} catch(IOException e) {
-							Comm.showError(Comm.COMMUNICATION_ERROR_STRING, e);
-						}
-					}
-				} catch (Exception e) {
-					Comm.showError(Document.getErrorString(e), e);
-					return;
+					socket = new Socket("127.0.0.1", 19876);
+					socket.close();
+					Comm.writeQueue.poll();
+					Comm.showError(Comm.OLD_VERSION_STRING, null);
+				} catch(Exception e1) {
+					Comm.writeQueue.poll();
+					Comm.showError(Comm.COMMUNICATION_ERROR_STRING, null);
 				}
+				return;
+			} catch (Exception e) {
+				Comm.writeQueue.poll();
+				Comm.showError(Document.getErrorString(e), e);
+				return;
 			}
-		} finally {
-			Comm.writeQueue.poll();
+			
+			try {
+				// Start write loop
+				while(true) {
+					CommFrame frame;
+					if(deferredFrame == null) {
+						try {
+							frame = Comm.writeQueue.take();
+						} catch (InterruptedException e) {
+							continue;
+						}
+					} else {
+						frame = deferredFrame;
+						deferredFrame = null;
+					}
+					
+					byte[] payload = frame.getBytes();
+					//ZoteroOpenOfficeIntegrationImpl.debugPrint("Writing "+new String(payload));
+					
+					try {
+						outputStream.writeInt(frame.getTransactionID());
+						outputStream.writeInt(payload.length);
+						outputStream.write(payload);
+						outputStream.flush();
+					} catch(SocketException e) {
+						deferredFrame = frame;
+						break;
+					} catch(IOException e) {
+						Comm.showError(Comm.COMMUNICATION_ERROR_STRING, e);
+					}
+				}
+			} catch (Exception e) {
+				Comm.writeQueue.poll();
+				Comm.showError(Document.getErrorString(e), e);
+				return;
+			}
 		}
 	}
 }
