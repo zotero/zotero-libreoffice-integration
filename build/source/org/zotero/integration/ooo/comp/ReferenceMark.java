ccf8370fb4a95d2e19af01350f73ddbfa2fec44c
Simon Kornblith <simon@simonster.com>
2013-02-17 21:53:15 -0500

    Remove code unlikely to be necessary

--- build/source/org/zotero/integration/ooo/comp/ReferenceMark.java
+++ build/source/org/zotero/integration/ooo/comp/ReferenceMark.java
@@ -225,19 +225,13 @@ public class ReferenceMark implements Comparable<ReferenceMark> {
 				dupRange.collapseToEnd();
 				dupRange.goRight((short) 1, true);
 				String str = dupRange.getString();
+				// getString() returns different newline characters for different OSes
 				if(str.equals("\n") || str.equals("\r\n")) {
-					dupRange.setString("");
-				} else if(str.equals("\r")) {
-					dupRange.goRight((short) 1, true);
 					dupRange.setString("");
 				}
 				dupRange.goLeft((short) 1, true);
 				str = dupRange.getString();
 				if(str.equals("\n") || str.equals("\r\n")) {
-					dupRange.setString("");
-				}
-				dupRange.goLeft((short) 1, true);
-				if(dupRange.getString().equals("\r")) {
 					dupRange.setString("");
 				}
 
