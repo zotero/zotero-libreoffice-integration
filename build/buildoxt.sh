1525b52d653656e474e41c28d482531269a013a6
Simon Kornblith <simon@simonster.com>
2013-07-09 13:05:40 -0400

    Remove unneeded types.rdb and idl files
    Fixes compatibility with LibreOffice 4.1

--- build/buildoxt.sh
+++ build/buildoxt.sh
@@ -5,18 +5,9 @@ LO_SDK_DIR=/usr/lib/libreoffice/sdk
 URE_DIR=/usr/lib/ure
 IDL_DIR=/usr/share/idl/libreoffice
 
-cd idl
-"$LO_SDK_DIR"/bin/idlc -O../build/urd/org/zotero/integration/ooo -I"$IDL_DIR" org/zotero/integration/ooo/*idl
-cd ..
-"$URE_DIR"/bin/regmerge types.rdb build/urd/org/zotero/integration/ooo/*urd /UCR 
-cd build
-"$LO_SDK_DIR"/bin/javamaker -BUCR ../types.rdb "$URE_DIR"/share/misc/types.rdb
-cd ..
-
 # Copy external jars
 mkdir -p oxt/external_jars
 cp -r lib/*jar oxt/external_jars
-cp -r types.rdb oxt/types.rdb
 
 # Fix MANIFEST.MF
 zip oxt/Zotero.jar META-INF/MANIFEST.MF
