zotero/zotero-libreoffice-integration

commit 1c2612a39ec583a3dc2b46c57abbf4a62a2b0133

Reverts 2deea6bd. Handles additional required setText() calls.
Previously integration.js used to handle additional RTF setText() calls for
setCode() invocations

components/zoteroOpenOfficeIntegration.js
@@ -478,6 +478,7 @@ var Field = function(documentID, index, code, noteIndex) {
 	this._index = index;
 	this._code = code;
 	this._noteIndex = noteIndex;
+	this._formattedText = null;
 };
 Field.prototype = {};
 
@@ -488,6 +489,13 @@ for (let method of ["delete", "select", "removeCode", "setText", "getText"]) {
 			[this._documentID, this._index].concat(Array.prototype.slice.call(arguments)));
 	};
 }
+Field.prototype.setText = function(text, isRich) {
+	if (isRich) {
+		this._formattedText = text;
+	}
+	return Comm.sendCommand("Field_setText",
+		[this._documentID, this._index, text, isRich]);
+}
 Field.prototype.getCode = function() {
 	Zotero.debug(`LibreOfficePlugin: Field(${this._index}).getCode: ${this._code}`);
 	return this._code;
@@ -496,6 +504,12 @@ Field.prototype.setCode = function(code) {
 	this._code = code;
 	Zotero.debug(`LibreOfficePlugin: Field(${this._index}).setCode: ${this._code}`);
 	Comm.sendCommand("Field_setCode", [this._documentID, this._index, code]);
+	// Setting field codes removes formatting, so we have to do reapply the text again
+	// (Doing this within the LO plugin is not viable)
+	if (this._formattedText) {
+		this.setText(this._formattedText, true);
+		this._formattedText = null;
+	}
 }
 Field.prototype.getNoteIndex = function() {
 	Zotero.debug(`LibreOfficePlugin: Field(${this._index}).getNoteIndex: ${this._noteIndex}`);

install.rdf
@@ -4,7 +4,7 @@
 <Description about="urn:mozilla:install-manifest">
 	<em:id>zoteroOpenOfficeIntegration@zotero.org</em:id>
 	<em:name>Zotero LibreOffice Integration</em:name>
-	<em:version>5.0.9.SOURCE</em:version>
+	<em:version>5.0.10.SOURCE</em:version>
 	<em:creator>Center for History and New Media<br/>George Mason University</em:creator>
 	<em:developer>Simon Kornblith</em:developer>
 	<em:homepageURL>https://www.zotero.org</em:homepageURL>

build/source/org/zotero/integration/ooo/comp/ReferenceMark.java
@@ -290,14 +290,6 @@ public class ReferenceMark implements Comparable<ReferenceMark> {
 		String oldRawCode = rawCode;
 		rawCode = Document.PREFIXES[0] + code + " RND" + Document.getRandomString(Document.REFMARK_ADD_CHARS);
 		doc.mMarkManager.renameMark(oldRawCode, rawCode);
-		// Setting the ReferenceMark name resets the style to the document default, so
-		// we store the previous style.
-		XPropertySet rangeProps = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, range);
-		Object[] oldPropertyValues = new Object[PROPERTIES_CHANGE_TO_DEFAULT.length];
-		for(int i=0; i<PROPERTIES_CHANGE_TO_DEFAULT.length; i++) {
-			Object result = rangeProps.getPropertyValue(PROPERTIES_CHANGE_TO_DEFAULT[i]);
-			oldPropertyValues[i] = result instanceof Any ? ((Any) result).getObject() : result;
-		}
 		
 		// Set the actual referenceMark code.
 		if(isTextSection) {
@@ -307,13 +299,7 @@ public class ReferenceMark implements Comparable<ReferenceMark> {
 			removeCode();
 			reattachMark();
 		}
-		
-		// And restore the style here
-		for(int i=0; i<PROPERTIES_CHANGE_TO_DEFAULT.length; i++) {
-			if(oldPropertyValues[i] != null) {
-				rangeProps.setPropertyValue(PROPERTIES_CHANGE_TO_DEFAULT[i], oldPropertyValues[i]);
-			}
-		}
+
 		getOutOfField();
 	}
 	

resource/installer.jsm
@@ -172,7 +172,7 @@ var Plugin = new function() {
 	}];
 	
 	// Bump if you want to trigger auto-update
-	this.LAST_INSTALLED_FILE_UPDATE = "5.0.9pre";
+	this.LAST_INSTALLED_FILE_UPDATE = "5.0.10pre";
 	this.DISABLE_PROGRESS_WINDOW = true;
 	
 	var zoteroPluginInstaller, pathToAddon, installing, prefBranch, wizardWindow;

commit 602e1e23432d34520060095879cc3bb55f8c9ac5

Fix citations losing formatting when code is updated w/o text updates
Regression since (1c2612a3).
Report
https://forums.zotero.org/discussion/comment/305860#Comment_305860

components/zoteroOpenOfficeIntegration.js
@@ -478,7 +478,6 @@ var Field = function(documentID, index, code, noteIndex) {
 	this._index = index;
 	this._code = code;
 	this._noteIndex = noteIndex;
-	this._formattedText = null;
 };
 Field.prototype = {};
 
@@ -490,9 +489,6 @@ for (let method of ["delete", "select", "removeCode", "setText", "getText"]) {
 	};
 }
 Field.prototype.setText = function(text, isRich) {
-	if (isRich) {
-		this._formattedText = text;
-	}
 	return Comm.sendCommand("Field_setText",
 		[this._documentID, this._index, text, isRich]);
 }
@@ -505,9 +501,21 @@ Field.prototype.setCode = function(code) {
 	Comm.sendCommand("Field_setCode", [this._documentID, this._index, code]);
 	// Setting field codes removes formatting, so we have to do reapply the text again
 	// (Doing this within the LO plugin is not viable)
-	if (this._formattedText) {
-		this.setText(this._formattedText, true);
-		this._formattedText = null;
+	let start = code.indexOf('{');
+	if (start == -1) {
+		return;
+	}
+	let json = JSON.parse(code.substring(start, code.lastIndexOf('}')+1));
+	let text = json.properties.formattedCitation;
+	let isRich = false;
+	if (text.includes("\\")) {
+		if (text.substr(0,5) != "{\\rtf") {
+			text = "{\\rtf "+text+"}";
+		}
+		isRich = true;
+	}
+	if (isRich) {
+		this.setText(text, isRich);
 	}
 }
 Field.prototype.getNoteIndex = function() {

install.rdf
@@ -4,7 +4,7 @@
 <Description about="urn:mozilla:install-manifest">
 	<em:id>zoteroOpenOfficeIntegration@zotero.org</em:id>
 	<em:name>Zotero LibreOffice Integration</em:name>
-	<em:version>5.0.12.SOURCE</em:version>
+	<em:version>5.0.13.SOURCE</em:version>
 	<em:creator>Center for History and New Media<br/>George Mason University</em:creator>
 	<em:developer>Simon Kornblith</em:developer>
 	<em:homepageURL>https://www.zotero.org</em:homepageURL>